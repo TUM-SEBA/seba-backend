0b9a68ed9b01c703482cedba19efa6bf3a386fbe
rriyaldhi <rriyaldhi@gmail.com>
2020-05-30 19:05:27 +0200

    Add Entity Route

---
+++ src/routes/entity.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const express  = require('express');
+const router   = express.Router();
+
+const middlewares    = require('../middlewares');
+const EntityController = require('../controllers/entity');
+
+
+router.get('/', EntityController.list); // List all entities
+router.post('/', middlewares.checkAuthentication, EntityController.create); // Create a new entity
+router.get('/:id', middlewares.checkAuthentication, EntityController.read); // Read a entity by Id
+router.put('/:id', middlewares.checkAuthentication, EntityController.update); // Update a entity by Id
+router.delete('/:id', middlewares.checkAuthentication, EntityController.remove); // Delete a entity by Id
+
+
+module.exports = router;
