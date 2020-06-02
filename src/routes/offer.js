aaefdb128e783c1a22f4bc0cf6574f335b8179ee
Swornim Shrestha <ge82lor@mytum.de>
2020-06-03 00:42:34 +0200

    Minor code changes in Routes of Offer

--- src/routes/offer.js
+++ src/routes/offer.js
@@ -10,7 +10,7 @@ const OfferController = require('../controllers/offer');
 router.get('/', OfferController.list); // List all offers
 router.post('/', middlewares.checkAuthentication, OfferController.create); // Create a new offer
 router.get('/:id', middlewares.checkAuthentication, OfferController.read); // Read an offer by Id
-router.get('/:userid', middlewares.checkAuthentication, OfferController.read); // Read an offer by Owner Id
+// router.get('/user/:userid', middlewares.checkAuthentication, OfferController.read); // Read an offer by Owner Id
 router.put('/:id', middlewares.checkAuthentication, OfferController.update); // Update an offer by Id
 router.delete('/:id', middlewares.checkAuthentication, OfferController.remove); // Delete an offer by Id
 
