c7b6d25ebf51a04f5acc8efc591ce7568620fc72
rriyaldhi <rriyaldhi@gmail.com>
2020-06-09 20:59:24 +0200

    Fix Entity & Review API

--- src/routes/review.js
+++ src/routes/review.js
@@ -7,7 +7,7 @@ const middlewares    = require('../middlewares');
 const ReviewController = require('../controllers/review');
 
 
-router.get('/', ReviewController.list); // List all reviews
+router.get('/', middlewares.checkAuthentication, ReviewController.list); // List all reviews
 router.post('/', middlewares.checkAuthentication, ReviewController.create); // Create a new review
 router.get('/:id', middlewares.checkAuthentication, ReviewController.read); // Read a review by Id
 router.put('/:id', middlewares.checkAuthentication, ReviewController.update); // Update a review by Id
