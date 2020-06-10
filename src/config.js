466c9cc73428ad22c8a79a5b4fe3ee10d9fe1862
Raymond Pinto <ge73lam@mytum.de>
2020-06-10 22:56:33 +0200

    Changed backend port from 3000 to 5000.

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 require('dotenv').config()
 
 // Configuration variables
-const port      = process.env.PORT        || '3000';
+const port      = process.env.PORT        || '5000';
 const mongoURI  = process.env.MONGODB_URI || 'mongodb://localhost:27010/communitydb';
 const JwtSecret = process.env.JWT_SECRET  || 'very secret secret';
 
