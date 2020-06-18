0c909700e58c24bc1f6342329183afa4d9539707
Eray Tufan <eray.tufan@siemens.com>
2020-06-18 23:15:37 +0200

    fixed spaces for classes to be accurate with the project style

--- src/controllers/biddingrequest.js
+++ src/controllers/biddingrequest.js
@@ -4,109 +4,109 @@ const BiddingRequestModel = require("../models/biddingrequest");
 const CustomerModel = require("../models/customer");
 
 const create = async (req, res) => {
-    if (!Object.prototype.hasOwnProperty.call(req.body, 'offer')) return res.status(400).json({
-        error: 'Bad Request',
-        message: 'The request body must contain a offer property'
+  if (!Object.prototype.hasOwnProperty.call(req.body, 'offer')) return res.status(400).json({
+    error: 'Bad Request',
+    message: 'The request body must contain a offer property'
+  });
+  if (!Object.prototype.hasOwnProperty.call(req.body, 'caretaker')) return res.status(400).json({
+    error: 'Bad Request',
+    message: 'The request body must contain a caretaker property'
+  });
+  if (Object.keys(req.body).length === 0)
+    return res.status(400).json({
+      error: "Bad Request",
+      message: "The request body is empty",
     });
-    if (!Object.prototype.hasOwnProperty.call(req.body, 'caretaker')) return res.status(400).json({
-        error: 'Bad Request',
-        message: 'The request body must contain a caretaker property'
-    });
-    if (Object.keys(req.body).length === 0)
-        return res.status(400).json({
-            error: "Bad Request",
-            message: "The request body is empty",
-        });
-    const username = req.body['caretaker'];
-    const customer = await CustomerModel.findOne({username}).exec();
-    let biddingRequest = req.body;
-    biddingRequest['caretaker'] = customer._id.toString();
-    BiddingRequestModel.create(biddingRequest)
-        .then((biddingRequest) => {
-            res.status(201).json(biddingRequest)
-        })
-        .catch((error) =>
-            res.status(500).json({
-                error: "Internal server error",
-                message: error.message,
-            })
-        );
+  const username = req.body['caretaker'];
+  const customer = await CustomerModel.findOne({username}).exec();
+  let biddingRequest = req.body;
+  biddingRequest['caretaker'] = customer._id.toString();
+  BiddingRequestModel.create(biddingRequest)
+    .then((biddingRequest) => {
+      res.status(201).json(biddingRequest)
+    })
+    .catch((error) =>
+      res.status(500).json({
+        error: "Internal server error",
+        message: error.message,
+      })
+    );
 };
 
 const read = (req, res) => {
-    BiddingRequestModel.findById(req.params.id)
-        .exec()
-        .then((biddingRequest) => {
-            if (!biddingRequest)
-                return res.status(404).json({
-                    error: "Not Found",
-                    message: `Bidding Request not found`,
-                });
+  BiddingRequestModel.findById(req.params.id)
+    .exec()
+    .then((biddingRequest) => {
+      if (!biddingRequest)
+        return res.status(404).json({
+          error: "Not Found",
+          message: `Bidding Request not found`,
+        });
 
-            res.status(200).json(biddingRequest);
-        })
-        .catch((error) =>
-            res.status(500).json({
-                error: "Internal Server Error",
-                message: error.message,
-            })
-        );
+      res.status(200).json(biddingRequest);
+    })
+    .catch((error) =>
+      res.status(500).json({
+        error: "Internal Server Error",
+        message: error.message,
+      })
+    );
 };
 
 const update = (req, res) => {
-    if (Object.keys(req.body).length === 0) {
-        return res.status(400).json({
-            error: "Bad Request",
-            message: "The request body is empty",
-        });
-    }
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).json({
+      error: "Bad Request",
+      message: "The request body is empty",
+    });
+  }
 
-    BiddingRequestModel.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-    })
-        .exec()
-        .then((biddingRequest) => res.status(200).json(biddingRequest))
-        .catch((error) =>
-            res.status(500).json({
-                error: "Internal server error",
-                message: error.message,
-            })
-        );
+  BiddingRequestModel.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  })
+    .exec()
+    .then((biddingRequest) => res.status(200).json(biddingRequest))
+    .catch((error) =>
+      res.status(500).json({
+        error: "Internal server error",
+        message: error.message,
+      })
+    );
 };
 
 const remove = (req, res) => {
-    BiddingRequestModel.findByIdAndRemove(req.params.id)
-        .exec()
-        .then(() =>
-            res
-                .status(200)
-                .json({message: `Bidding request with id${req.params.id} was deleted`})
-        )
-        .catch((error) =>
-            res.status(500).json({
-                error: "Internal server error",
-                message: error.message,
-            })
-        );
+  BiddingRequestModel.findByIdAndRemove(req.params.id)
+    .exec()
+    .then(() =>
+      res
+        .status(200)
+        .json({message: `Bidding request with id${req.params.id} was deleted`})
+    )
+    .catch((error) =>
+      res.status(500).json({
+        error: "Internal server error",
+        message: error.message,
+      })
+    );
 };
 
 const list = (req, res) => {
-    BiddingRequestModel.find({})
-        .exec()
-        .then((biddingRequests) => res.status(200).json(biddingRequests))
-        .catch((error) =>
-            res.status(500).json({
-                error: "Internal server error",
-                message: error.message,
-            })
-        );
+  BiddingRequestModel.find({})
+    .exec()
+    .then((biddingRequests) => res.status(200).json(biddingRequests))
+    .catch((error) =>
+      res.status(500).json({
+        error: "Internal server error",
+        message: error.message,
+      })
+    );
 };
 
 module.exports = {
-    create,
-    read,
-    update,
-    remove,
-    list,
+  create,
+  read,
+  update,
+  remove,
+  list,
 };
