4945695ebf5af91c5efd900149a4583032a85886
Eray Tufan <eray.tufan@siemens.com>
2020-07-12 16:46:22 +0200

    updated review backend calls

--- src/routes/review.js
+++ src/routes/review.js
@@ -12,6 +12,7 @@ router.post('/', middlewares.checkAuthentication, ReviewController.create); // C
 router.get('/:id', middlewares.checkAuthentication, ReviewController.read); // Read a review by Id
 router.put('/:id', middlewares.checkAuthentication, ReviewController.update); // Update a review by Id
 router.delete('/:id', middlewares.checkAuthentication, ReviewController.remove); // Delete a review by Id
-router.get('/user/listByOwnerId', middlewares.checkAuthentication, ReviewController.listByOwnerId); // Read a review by Owner Id
+router.get('/user/listByCaretakerId', middlewares.checkAuthentication, ReviewController.listByMyCaretakerId); // Read a review by Owner Id
+router.get('/user/:id', middlewares.checkAuthentication, ReviewController.listByCaretakerId); // Read a review by Owner Id
 
 module.exports = router;
